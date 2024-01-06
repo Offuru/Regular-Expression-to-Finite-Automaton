37f0291cc31f44a14721658a429819ec9d6d8bad
Constantin Voicu <voicu.constantin@outlook.com>
2024-01-06 20:27:16 +0200

    Added isValidRegex

--- Project/Project/Project.cpp
+++ Project/Project/Project.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include <fstream>
+#include <stack>
 #include "AFD.h"
+
+bool isValidRegex(std::string regex);
 
 int main()
 {
@@ -12,4 +16,39 @@ int main()
 	afd1++;	
 
 	std::cout << afd1;
+}
+
+bool isValidRegex(std::string regex)
+{
+	std::stack<char> parenthesesStack;
+	for (auto itChr = regex.cbegin(); itChr != regex.cend(); ++itChr)
+	{
+		if (*itChr == '(')
+		{
+			parenthesesStack.push(*itChr);
+
+			if (itChr + 1 != regex.cend())
+			{
+				if (*(itChr + 1) == '|') return false;
+				if (*(itChr + 1) == '*') return false;
+				if (*(itChr + 1) == '.') return false;
+			}
+		}
+		else if (*itChr == ')')
+			parenthesesStack.pop();
+		else
+		{
+			if (*itChr == '.' && itChr + 1 == regex.cend()) return false;
+			if (*itChr == '|' && itChr + 1 == regex.cend()) return false;
+			if (itChr + 1 != regex.cend())
+			{
+				if (*itChr == '.' && (*(itChr + 1) == '.' || *(itChr + 1) == '|' || *(itChr + 1) == '*' || *(itChr + 1) == ')' || itChr == regex.cbegin())) return false;
+				if (*itChr == '*' && (*(itChr + 1) == '*' || itChr == regex.cbegin())) return false;
+				if (*itChr == '|' && (*(itChr + 1) == '*' || *(itChr + 1) == ')' || *(itChr + 1) == '.' || itChr == regex.cbegin())) return false;
+			}
+
+		}
+	}
+	if (!parenthesesStack.empty()) return false;
+	return true;
 }

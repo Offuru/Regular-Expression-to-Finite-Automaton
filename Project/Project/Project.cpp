4ffa2d5b868852e75fabf864eacf157756228cca
Constantin Voicu <voicu.constantin@outlook.com>
2024-01-07 12:18:43 +0200

    Added menu Changed checkWord function

--- Project/Project/Project.cpp
+++ Project/Project/Project.cpp
@@ -5,8 +5,12 @@
 
 bool isValidRegex(std::string regex);
 
+void printMenu();
+
 int main()
 {
+	std::ifstream fin("input.txt");
+
 	AFD afd1('a');
 	AFD afd2('b');
 	AFD afd3('c');
@@ -15,10 +19,58 @@ int main()
 	afd1 |= afd3;
 	afd1++;	
 
-	AFD regex("aba(aa|bb)*c(ab)*");
-	//std::cout << regex;
-	std::cout << regex.checkWord("abacab");
 	//std::cout << afd1;
+
+	std::string regex;
+	fin >> regex;
+
+	if (isValidRegex(regex))
+	{
+		int answear = 1;
+		AFD automaton(regex);
+
+		printMenu();
+		std::ofstream fout("output.txt");
+		while (answear)
+		{
+			std::cin >> answear;
+			std::string word;
+
+			switch (answear)
+			{
+				case 1:
+					std::cout << automaton << "\n\n";
+					break;
+
+				case 2:
+					fout << automaton << "\n\n";
+					break;
+
+				case 3:
+					std::erase(regex, '.');
+					std::cout << regex << "\n\n";
+					break;
+
+				case 4:
+					std::cout << "Type the word: ";
+					std::cin >> word;
+					if (automaton.checkWord(word))
+						std::cout << "The word is valid!\n\n";
+					else
+						std::cout << "The word is not valid!\n\n";
+					break;
+
+				case 0: 
+					return 0;
+
+				default:
+					break;
+			}
+			printMenu();
+		}
+	}
+
+	return 0;
 }
 
 bool isValidRegex(std::string regex)
@@ -54,4 +106,13 @@ bool isValidRegex(std::string regex)
 	}
 	if (!parenthesesStack.empty()) return false;
 	return true;
+}
+
+void printMenu()
+{
+	std::cout << "1: Show the automaton in console;\n";
+	std::cout << "2: Show the automaton in file;\n";
+	std::cout << "3: Show intelligible regex;\n";
+	std::cout << "4: Check word;\n";
+	std::cout << "0: EXIT.\n";
 }
